96e037e3ddd83335a769c4c871a51d17e750d09f
ReDOPT <daniacos95@gmail.com>
2024-09-04 11:38:22 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,5 @@
+let username = prompt("What is your name?");
+let email = prompt("What is your email address?")
+let userage = prompt("What is your age number?");
+
+console.log(`name ${username}, email ${email},age ${userage}`);
