bc197275d81233aabf4708730cbca571e24314a6
ReDOPT <daniacos95@gmail.com>
2024-09-08 20:03:40 +0300

    Initial commit

--- main.js
+++ main.js
@@ -1,21 +1,39 @@
-let stringnamber =prompt("enter a five-digit number");
-let realnumber = +stringnamber
+let userAge = prompt("enter a age");
+let userCity = prompt("enter a city");
+let userSport = prompt("your favorite sport");
 
-let thirdnumber =realnumber % 10
-let secondnumber =Math.floor(realnumber / 10 % 10)
-let firstnumber =Math.floor(realnumber / 100 % 10)
-
-if(thirdnumber === secondnumber && thirdnumber === firstnumber ){
-    console.log(`Всі цифри однакові`)
-}else if(
-    thirdnumber === secondnumber || thirdnumber === firstnumber || secondnumber === firstnumber
-) {
-    console.log(`є однакові цифри`)
-
+if (userAge == null ) {
+    console.log(`It’s a pity that you didn’t want to enter your age`);
 }else {
-    console.log(`немає однакових цифр`)
+    console.log(`your age ${userAge}`)
 }
 
+if (userCity == null ) {
+    console.log(`It’s a pity that you didn’t want to enter your city`);
+}else if ( userCity === `Kiev`
+    ||
+    userCity === `Washington`
+    ||
+    userCity === `London`
+    ||
+    userCity === `Вашингтон`
+    ||
+    userCity === `Київ`
+    ||
+    userCity === `Лондон`  ){
+    console.log(`you live in the capital ${userCity}`)
+}else{
+    console.log(`do you live in the city ${userCity}`)
+}
 
-
-console.log( realnumber);
+if(userSport == null){
+    console.log(`It’s a pity that they didn’t want to introduce your favorite sport`)
+}else if (userSport=== `football`){
+    console.log(`Cool! Do you want Karim Benzema?`)
+}else if(userSport===`basketball`){
+    console.log(`Cool! Would you like Michael Jordan?`)
+}else if(userSport===`Cool! Do you want Jaromir Jagr?`){
+    console.log(`hockey ${userSport}`)
+}else {
+    console.log(`wonderful sport ${userSport}`)
+}
