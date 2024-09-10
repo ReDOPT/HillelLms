41734135cc1aa073f2135d59ae0159b523f432b8
ReDOPT <daniacos95@gmail.com>
2024-09-10 19:33:17 +0300

    Initial commit

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let userSport = prompt("your favorite sport");
 if (userAge == null ) {
     console.log(`It’s a pity that you didn’t want to enter your age`);
 }else {
-    console.log(`your age ${2024-userAge}`)
+    console.log(`your age ${userAge-2024}`)
 }
 
 if (userCity == null ) {
