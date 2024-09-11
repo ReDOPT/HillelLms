789e84254a6ef4cbfa93cee07dfaa294cc61ecdd
ReDOPT <daniacos95@gmail.com>
2024-09-11 13:06:26 +0300

    Initial commit

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
-for(let i= 10 ; i <= 100 ; i += 10  ) {
-    console.log(`$${i}`)
-    console.log(`₴${i*26}`)
-    console.log("")
+let n = prompt(`number N`)
+let result = n
+
+for (let i = 1; i <=n; i++){
+    result  = result*n
+    if ( result <= 100 && result % 2 === 0 ){
+        console.log(result)
+    }
+}
+
+console.log()
+
+let squareOfTheNumber = n*=n
+for ( let i=1 ; i<=squareOfTheNumber ; i++ ){
+    if ( i<= 100 && i%2 === 0 ){
+        console.log(i)
+    }
 }
