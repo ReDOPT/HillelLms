1813476a2d578f05f6e0c2aad8c6e39705e28319
ReDOPT <daniacos95@gmail.com>
2024-09-15 12:41:03 +0300

    Initial commit

--- main.js
+++ main.js
@@ -2,18 +2,32 @@ let str =prompt(`Enter sting`,`hello world`)
 let deletLeter =prompt(`first letter to delete`)
 let deletLeterT =prompt(`second letter to delete`)
 
-let strArray =str.split(``)
 
-for (let i = 0; i < strArray.length  ; i++) {
-    if (deletLeter === `` || deletLeterT === `` || deletLeterT === null) {
-        `Ви не ввели букви для видалення`
-        break
-    }else if (strArray[i] === deletLeter) {
-        strArray[i] =``
-    }else if (strArray[i] === deletLeterT) {
-        strArray[i] =``
+
+function deletLeterString( str,deletLeter ,deletLeterT){
+    let strArray =str.split(``)
+
+    for (let i = 0; i < strArray.length  ; i++) {
+
+        if (deletLeter === `` || deletLeter === null) {
+            console.log(`Ви не ввели першу букви для видалення`)
+            break
+        }else if (strArray[i] === deletLeter) {
+            strArray[i] =``
+        }
     }
+
+    for (let i = 0; i < strArray.length  ; i++){
+        if(deletLeterT === `` ||deletLeterT ===null){
+            console.log(`Ви не ввели другу букви для видалення`)
+            break
+        }else if (strArray[i] === deletLeterT) {
+            strArray[i] =``
+        }
+    }
+
+    let arrayStr = strArray.join(``)
+    return arrayStr;
 }
-let arrayStr = strArray.join(``)
+let strArray = deletLeterString(str,deletLeter,deletLeterT)
 console.log(strArray)
-console.log(arrayStr)
