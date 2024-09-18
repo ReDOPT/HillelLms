32021ca1e320458aa9d4233f07d3a8467adc83e8
ReDOPT <daniacos95@gmail.com>
2024-09-18 14:44:42 +0300

    Initial commit

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-function sum (firstNumber){
-    return function(secondnumber){
-        return firstNumber * secondnumber
+function inputuser() {
+    for (let i = 1 ; i <= 10;i++){
+        let userPromt =prompt()
+
+        if(userPromt>100){
+            return userPromt
+            break
+        }else if(i === 9){
+            alert(`у вас осталась одна попытка`)
+        }else{
+            alert("ne virno")
+        }
     }
 }
-
-console.log(sum(3)(5))
+console.log(inputuser())
