1a8c2f9c5c8cae78c26c07ed45baad993317a242
ReDOPT <daniacos95@gmail.com>
2024-10-04 12:44:38 +0300

    Initial commit

--- main.js
+++ main.js
@@ -1,7 +1,34 @@
-const numbers = [1231, 122, 233, 34, 45, 64, 27, 2348, 249, 210];
 
-const evenNumbers = numbers.filter(function(number) {
-    return number % 2 === 0;
-});
+const contactBook = {
+    contacts: [
 
-console.log(evenNumbers);
+        {   name: 'Іван Іванов',
+            phone: '+380501234567',
+            email: 'ivan@example.com'
+        },
+        {   name: 'Ольга Петрова',
+            phone: '+380971234567',
+            email: 'olga@example.com'
+        }
+    ],
+
+
+    findContactByName: function(name) {
+        const contact = this.contacts.find(contact => contact.name.toLowerCase() === name.toLowerCase());
+        return contact ? contact : 'Контакт не знайдено';
+    },
+
+
+    addContact: function(name, phone, email) {
+        this.contacts.push({ name: name, phone: phone, email: email });
+        console.log('Контакт успішно додано');
+    }
+};
+
+contactBook.addContact('Марія Сидорова', '+380631234567', 'maria@example.com');
+
+
+const foundContact = contactBook.findContactByName('Іван Іванов');
+console.log(foundContact);
+
+console.log(contactBook.contacts);
