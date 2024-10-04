e6a8124c2be26a421fef293f184ec120b0cd54a9
ReDOPT <daniacos95@gmail.com>
2024-10-04 12:34:52 +0300

    Initial commit

--- main.js
+++ main.js
@@ -1,18 +1,7 @@
-const users ={
-    name: 'Іван',
-    age: 27,
-    location: 'Днепр, Україна',
-    email: 'ivan@example.com',
-    password: '123456',
+const numbers = [1231, 122, 233, 34, 45, 64, 27, 2348, 249, 210];
 
-    getUserInfo: function () {
-        console.log(`Ім'я: ${this.name}`);
-        console.log(`Вік: ${this.age}`);
-        console.log(`Місце проживання: ${this.location}`);
-        console.log(`Електронна пошта: ${this.email}`);
-        console.log(`пороль: ${this.password}`)
-    }
+const evenNumbers = numbers.filter(function(number) {
+    return number % 2 === 0;
+});
 
-}
-
-users.getUserInfo()
+console.log(evenNumbers);
