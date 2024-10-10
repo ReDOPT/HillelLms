8652acb1b7dd17e6f04dfe411a175e81317ff7c3
ReDOPT <daniacos95@gmail.com>
2024-10-10 15:35:32 +0300

    Initial commit

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
-document.querySelector('#mydiv').addEventListener('click', (e) => {
-    alert(`ви натиснули ${e.target.textContent}`);
-})
+const todoInput = document.querySelector("#inputtodo");
+const addTodoBtn = document.querySelector("#btnenter");
+const todoList = document.querySelector("#todolist");
+
+let userInput = "";
+
+todoInput.addEventListener('input',(e) =>{
+    userInput = e.target.value;
+});
+
+addTodoBtn.addEventListener('click',() =>{
+    console.log(userInput);
+    const newTodo = document.createElement('div');
+    newTodo.textContent = userInput;
+    newTodo.classList.add('todo');
+
+    todoList.appendChild(newTodo);
+
+    todoInput.value ='';
+
+    const todoBtn = document. createElement('button');
+    todoBtn.textContent = 'X';
+    todoBtn.classList.add('btn_Enter')
+    newTodo.appendChild(todoBtn)
+
+    todoBtn.addEventListener('click', (e)=>{
+        console.log(e.target.parentNode);
+        e.target.parentNode.remove()
+    })
+});
