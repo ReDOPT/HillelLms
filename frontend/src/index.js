26d6268b7368c336470bd5eba8b65da7aeea5508
ReDOPT <daniacos95@gmail.com>
2024-12-01 18:31:33 +0200

    Delete .idea directory

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,3 +6,29 @@ async function getTodo() {
 };
 
 getTodo()
+
+const ul = document.querySelector(".form__ul--js")
+const input = document.querySelector(".js--form__input").addEventListener('input', (e)=>{
+    userInput = e.target.value;
+});
+
+let todoSave = JSON.parse(localStorage.getItem('todo-save')) || [];
+
+let userInput = "" ;
+
+function ulBild() {
+
+}
+
+document.querySelector(".form__btn--js").addEventListener('click',(e)=>{
+   e.preventDefault()
+
+    if(userInput.trim() !== ""){
+        ulBild(userInput,false);
+
+    }
+});
+
+function saveTodoToLocalStorage() {
+    localStorage.setItem('todo-save', JSON.stringify(todoSave));
+}
